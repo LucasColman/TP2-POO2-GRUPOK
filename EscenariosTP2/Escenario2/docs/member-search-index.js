fca22566adbc49186145386f2b8ffb91a40467a3
Lucas Colman <lukasscolman@gmail.com>
2024-10-12 19:16:13 -0300

    docs Escenario2

---
+++ EscenariosTP2/Escenario2/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Obsoleto","l":"nuevoMetodo()"},{"p":"<Unnamed>","c":"Obsoleto","l":"Obsoleto()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Obsoleto","l":"viejoMetodo()"}];updateSearchResults();
