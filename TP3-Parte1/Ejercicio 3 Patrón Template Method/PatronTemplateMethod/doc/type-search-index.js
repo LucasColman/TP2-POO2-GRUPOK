ffa6d1bfda65eed410a95021bf2d922456f04ccb
Alejandro Martinez <martinezalejandrouni10@gmail.com>
2024-10-24 20:42:03 -0300

    Documentacion para TP3 - Ejercicio 3.

---
+++ TP3-Parte1/Ejercicio 3 Patrón Template Method/PatronTemplateMethod/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cakefactory","l":"App"},{"p":"cakefactory","l":"Cake"},{"p":"cakefactory","l":"ChocolateCake"},{"p":"cakefactory","l":"VanillaCake"}];updateSearchResults();
