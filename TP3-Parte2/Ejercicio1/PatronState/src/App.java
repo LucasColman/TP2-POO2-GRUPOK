e639874ee4db0a8c05bbe371a7a77b1fcedd224a
Lucas Colman <lukasscolman@gmail.com>
2024-11-02 18:39:56 -0300

    TP3 Parte2

---
+++ TP3-Parte2/Ejercicio1/PatronState/src/App.java
@@ -0,0 +1,21 @@
+/*
+1. Patrón State - Gestión de Semáforos 
+
+Enunciado: Imagina que trabajas en el desarrollo de un sistema de control de tráfico. Un 
+semáforo puede tener tres estados: Rojo, Amarillo, y Verde. El comportamiento del 
+semáforo cambia dependiendo de su estado actual (por ejemplo, cuando está en rojo, los 
+autos deben detenerse; cuando está en verde, pueden avanzar). 
+
+Tareas: 
+     Implementa el patrón State para modelar los diferentes estados del semáforo. 
+     El sistema debe poder cambiar entre los diferentes estados de manera controlada. 
+     Incluir un diagrama UML y el código en Java que represente la solución.
+ */
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ejercicio1dasdasdasd");
+    }
+}
---
+++ TP3-Parte2/Ejercicio2/PatronStrategy/src/App.java
@@ -0,0 +1,23 @@
+
+/*
+ 2. Patrón Strategy - Métodos de Envío en una Tienda en Línea 
+
+ Enunciado: Estás desarrollando un sistema para una tienda en línea. Cuando un cliente 
+realiza una compra, puede elegir entre diferentes métodos de envío: Envío Regular, Envío 
+Expreso o Envío Internacional. Cada método tiene un costo y una estrategia de entrega 
+diferente. 
+
+Tareas: 
+     Implementa el patrón Strategy para manejar los diferentes métodos de envío. 
+     El sistema debe permitir cambiar fácilmente entre las diferentes estrategias de envío. 
+     Incluir un diagrama UML y el código en Java que represente la solución. 
+
+ */
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
