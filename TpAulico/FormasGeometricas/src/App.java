17b95c1da53978e83ea5d9bf1725b4b98cddafbd
Alejandro Martinez <martinezalejandrouni10@gmail.com>
2024-11-08 19:37:45 -0300

    Java doc y Diagrama de clases

--- TpAulico/FormasGeometricas/src/App.java
+++ TpAulico/FormasGeometricas/src/App.java
@@ -2,16 +2,31 @@ import models.Circulo;
 import models.GrupoForma;
 import models.Rectangulo;
 
+/**
+ * La clase principal `App` ejecuta la aplicación de dibujo de formas geométricas.
+ * Crea instancias de varias formas geométricas y las añade a un grupo para luego dibujarlas.
+ */
 public class App {
+
+    /**
+     * Método principal de la aplicación. Crea instancias de `Circulo`, `Rectangulo` y
+     * `GrupoForma`, agrega las formas geométricas al grupo y llama al método para
+     * dibujarlas en conjunto.
+     *
+     * @param args Argumentos de línea de comandos (no se utilizan).
+     * @throws Exception si ocurre alguna excepción durante la ejecución.
+     */
     public static void main(String[] args) throws Exception {
+        // Crear instancias de las formas geométricas individuales
         Circulo circulo = new Circulo();
         Rectangulo rectangulo = new Rectangulo();
+
+        // Crear un grupo de formas y agregar las formas geométricas al grupo
         GrupoForma grupo = new GrupoForma();
-        
         grupo.agregar(circulo);
         grupo.agregar(rectangulo);
 
+        // Dibujar todas las formas en el grupo
         grupo.dibujar();
-
     }
 }
--- TpAulico/FormasGeometricas/src/models/Circulo.java
+++ TpAulico/FormasGeometricas/src/models/Circulo.java
@@ -1,11 +1,25 @@
 package models;
-// Circulo.java
+
+/**
+ * La clase Circulo representa una forma geométrica de tipo círculo
+ * e implementa la interfaz FormaGeometrica. Esta clase define cómo
+ * se debe dibujar un círculo.
+ */
 public class Circulo implements FormaGeometrica {
 
+    /**
+     * Constructor por defecto de la clase Circulo.
+     * No recibe parámetros y simplemente crea una instancia de Circulo.
+     */
     public Circulo() {}
 
+    /**
+     * Método para dibujar el círculo.
+     * Implementación de la interfaz FormaGeometrica que muestra un mensaje en consola.
+     */
     @Override
     public void dibujar() {
-        System.out.println("Dibujando un circulo");
+        System.out.println("Dibujando un círculo");
     }
 }
+
