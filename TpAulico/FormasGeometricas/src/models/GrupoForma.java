571a04c9a3420f3f4d49588f6efbc1dd4b998999
Mariano <mivasiuta@socialnet.com.ar>
2024-11-08 19:32:08 -0300

    doc rectangulo grupoForma

--- TpAulico/FormasGeometricas/src/models/GrupoForma.java
+++ TpAulico/FormasGeometricas/src/models/GrupoForma.java
@@ -1,20 +1,36 @@
 package models;
 
-// GrupoForma.java
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * La clase GrupoForma implementa la interfaz FormaGeometrica y representa un grupo de formas geométricas.
+ */
 public class GrupoForma implements FormaGeometrica {
     private List<FormaGeometrica> formas = new ArrayList<>();
-
+    
+    /**
+     * Agrega una forma geométrica al grupo.
+     * 
+     * @param forma la forma geométrica a agregar
+     */
     public void agregar(FormaGeometrica forma) {
         formas.add(forma);
     }
 
+    /**
+     * Remueve una forma geométrica del grupo.
+     * 
+     * @param forma la forma geométrica a remover
+     */
     public void remover(FormaGeometrica forma) {
         formas.remove(forma);
     }
 
+    /**
+     * Dibuja todas las formas geométricas del grupo.
+     * Implementa el método dibujar de la interfaz FormaGeometrica.
+     */
     @Override
     public void dibujar() {
         System.out.println("Dibujando grupo de formas:");
